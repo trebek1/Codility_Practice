d3af3033d59eb5f0d1428e4eee70c41545e883d2
trebek1 <trebek08@gmail.com>
2015-07-20 22:58:32 -0700

    added heaps method to find permutations

--- permutations.js
+++ permutations.js
@@ -13,3 +13,39 @@ function doPerm(str, arr) {
 }
 doPerm('', []);
 console.log(permutations);
+
+// Heaps Method of Permutations 
+
+function swap(chars, i, j) {
+    var tmp = chars[i];
+    chars[i] = chars[j];
+    chars[j] = tmp;
+}
+
+function getAnagrams(input) {
+    var counter = [],
+        anagrams = [],
+        chars = input.split(''),
+        length = chars.length,
+        i;
+
+    for (i = 0; i < length; i++) {
+        counter[i] = 0;
+    }
+
+    anagrams.push(input);
+    i = 0;
+    while (i < length) {
+        if (counter[i] < i) {
+            swap(chars, i % 2 === 1 ? counter[i] : 0, i);
+            counter[i]++;
+            i = 0;
+            anagrams.push(chars.join(''));
+        } else {
+            counter[i] = 0;
+            i++;
+        }
+    }
+
+    return anagrams;
+}
