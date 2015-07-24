30ab50e02735b3fb212cb3d7b6e724e555225f6b
trebek1 <trebek08@gmail.com>
2015-07-24 01:12:26 -0700

    revised notes

--- cake_thief.js
+++ cake_thief.js
@@ -9,7 +9,7 @@ var capacity    = 20
 
 var max_duffel_bag_value = function(cake_tuples, capacity){
     // Make an array size capacity +1 to hold optimal weights for each capacity 0 through Capacity
-    var final = []; 
+    var final = [0]; 
     for(var i =0; i<=capacity; i++){
         final[i] = 0; 
     }
@@ -25,9 +25,10 @@ var max_duffel_bag_value = function(cake_tuples, capacity){
                 var remainder = (i-(Math.floor(i/cake_tuples[j][0]))*cake_tuples[j][0])||0;
                 // lookup remaining capacity in the sack and use the optimal capacity that we found for this amount before 
                 var remainder_lookup = final[remainder];
+                // max value is the value of using the cakes + the remainder optimized with previous optimal amount
                 var max_value_using_cake = cake + remainder_lookup; 
-              // the new position is the max of using the new cake or the last value + a cake of 1, or last cake and zero if that remainder has not been defined  
-                final[i] = Math.max(max_value_using_cake, (final[i-1] + (final[1] || 0)), final[i]); 
+              // the new position is the max of using the new cake or the capacity before (i-1) + a cake of 1 if it exists, or a final value defined with an earlier cake in the cake_tuple array this pass. final[1] || 0 in case final[1] is not defined 
+                final[i] = Math.max(max_value_using_cake, (final[i-1] + final[1] || 0), final[i]); 
                 
             }
         }
