e680bbca893a00720002b16585db64a668645e6f
trebek1 <trebek08@gmail.com>
2015-07-25 19:56:36 -0700

    100% minPermeterRectangle

---
+++ minPerimeterRectangle.js
@@ -0,0 +1,37 @@
+// An integer N is given, representing the area of some rectangle.
+// The area of a rectangle whose sides are of length A and B is A * B, and the perimeter is 2 * (A + B).
+// The goal is to find the minimal perimeter of any rectangle whose area equals N. The sides of this rectangle should be only integers.
+// For example, given integer N = 30, rectangles of area 30 are:
+// (1, 30), with a perimeter of 62,
+// (2, 15), with a perimeter of 34,
+// (3, 10), with a perimeter of 26,
+// (5, 6), with a perimeter of 22.
+// Write a function:
+// function solution(N);
+// that, given an integer N, returns the minimal perimeter of any rectangle whose area is exactly equal to N.
+// For example, given an integer N = 30, the function should return 22, as explained above.
+// Assume that:
+// N is an integer within the range [1..1,000,000,000].
+// Complexity:
+// expected worst-case time complexity is O(sqrt(N));
+// expected worst-case space complexity is O(1).
+
+// you can use console.log for debugging purposes, i.e.
+// console.log('this is a debug message');
+
+function solution(N) {
+    // write your code in JavaScript (Node.js 0.12)
+    var limit = Math.sqrt(N); 
+    var min = 10*N; 
+    for(var i = 1; i<=limit; i++){
+    
+    if(N/i === Math.floor(N/i)){
+    var P = 2*((N/i) + i); 
+        if(P<min){
+        min = P; 
+        }
+    }
+    
+    }
+    return min; 
+}
